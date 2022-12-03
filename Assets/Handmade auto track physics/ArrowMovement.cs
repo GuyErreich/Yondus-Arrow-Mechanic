43f18c92b5ef10d9e6a043dc7216c48e31b860bf
GuyErreich <guyerreich@gmail.com>
2022-12-04 00:38:25 +0200

    Modified ArrowMovement for minimum testing POC

--- Assets/Handmade auto track physics/ArrowMovement.cs
+++ Assets/Handmade auto track physics/ArrowMovement.cs
@@ -7,14 +7,36 @@ public class ArrowMovement : MonoBehaviour
 {
     [SerializeField] private List<Transform> points;
     [SerializeField, Range(1, 2)] private float force = 1f;
+    [SerializeField, Range(1, 4)] private float speed = 1f;
 
     private ArrowPath path;
 
-    private IEnumerable Shoot() {
+    private void Awake() {
+        this.ResetPath();
+    }
 
-        if (this.path == null || path.NumOfSegments == 0) return;
+    private void ResetPath()
+    {
+        if(points == null || points.Count == 0)
+            return;
+        
+        var direction = this.transform.forward;
+        var velocity = (this.points[0].position - this.transform.position).magnitude * 0.5f;
+        this.path = new ArrowPath(this.transform.position, this.points[0].position, direction, velocity);
+        
+        for (var i = 1; i < this.points.Count; i++) {
+            this.path.AddSegment(this.points[i].position, this.force);
+        }
+    }
 
-        var positions = new List<Vector3>();
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    private IEnumerator Shoot() {
+        if (this.path == null || path.NumOfSegments == 0) yield return null;
 
         var segmentIndex = 0;
 
@@ -23,15 +45,14 @@ public class ArrowMovement : MonoBehaviour
 
             while (t <= 1) {
                 var segmentPoints = this.path.GetPointsInSegment(segmentIndex);
-                positions.Add(BezireCurve.Cubic(segmentPoints[0], segmentPoints[1], segmentPoints[2], segmentPoints[3], t));
+                this.transform.position = BezireCurve.Cubic(segmentPoints[0], segmentPoints[1], segmentPoints[2], segmentPoints[3], t);
 
-                // t += this.precision;
+                yield return new WaitForEndOfFrame();
+
+                t += speed * Time.deltaTime;
             }
 
             segmentIndex++;
         }
-        
-        this.line.positionCount = positions.Count;
-        this.line.SetPositions(positions.ToArray());
     }
 }
