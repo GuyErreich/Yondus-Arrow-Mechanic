9c205aba11933ec1b48dfe94a2e555e9838dbcbf
GuyErreich <guyerreich@gmail.com>
2023-01-24 00:06:36 +0200

    Minor Modifications

--- Assets/YundosArrow/Scripts/Player/Combat/ArrowAbillity/Actions/Damage.cs
+++ Assets/YundosArrow/Scripts/Player/Combat/ArrowAbillity/Actions/Damage.cs
@@ -1,15 +1,9 @@
 using UnityEngine;
 using YundosArrow.Scripts.Enemy;
+using YundosArrow.Scripts.Player.Combat.ArrowAbilities.Utils;
 
-namespace YundosArrow.Scripts.Systems {
+namespace YundosArrow.Scripts.Player.Combat.ArrowAbilities.Actions {
     public class Damage : MonoBehaviour, ISerializationCallbackReceiver {
-        [System.Serializable]
-        private struct col {
-            [SerializeField] public Vector3 center;
-            [SerializeField] public float radius;
-        }
-
-        [SerializeField] private new col collider;
         [SerializeField] private float amount;
         [SerializeField] private LayerMask layerMask;
 
@@ -24,17 +18,10 @@ namespace YundosArrow.Scripts.Systems {
             if (other.tag == "Enemy") {
                 var health = other.GetComponent<Health>();
                 if (health) {
-                    health.Change(-(this.Amount));
+                    if (GlobalCollections.Targets.Contains(other.transform))
+                        health.Change(-(this.Amount));
                 }
             }
-        }
-
-        private void OnDrawGizmosSelected() {
-
-            Gizmos.matrix = this.transform.localToWorldMatrix;
-            Gizmos.color = Color.green;
-            // var pos = this.transform.right * this.collider.center.x + this.transform.up * this.collider.center.y + this.transform.forward * this.collider.center.z;
-            Gizmos.DrawWireSphere(this.collider.center, this.collider.radius);
         }
 
         public void OnBeforeSerialize()
