d9a6fc237c9057ccd6b784dbcde65b5b0b6acf4f
GuyErreich <guyerreich@gmail.com>
2023-01-28 11:55:28 +0200

    Better Bezeir curve movement: Now using the full mathematic formula of cubic bezier curve + derivatives + arc length

--- Assets/YundosArrow/Scripts/Player/Combat/ArrowAbillity/Actions/ArrowMovement.cs
+++ Assets/YundosArrow/Scripts/Player/Combat/ArrowAbillity/Actions/ArrowMovement.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using YundosArrow.Scripts.Player.Combat.ArrowAbilities.Utils;
-using YundosArrow.Scripts.Systems;
 
 namespace YundosArrow.Scripts.Player.Combat.ArrowAbilities.Actions
 {
@@ -28,28 +26,31 @@ namespace YundosArrow.Scripts.Player.Combat.ArrowAbilities.Actions
             IsMoving = true;
             
             var t = 0f;
-            var distance = 0f;
+            // var distance = 0f;
             ArrowPath path = null;
 
             while (targets.Count > 0) {
                 t = 0f;
                 var currentPos = ArrowStats.Arrow.position;
-                distance = Vector3.Distance(currentPos, targets[0].position);
 
                 if (path == null)
                     path = new ArrowPath(ArrowStats.Arrow.position, targets[0].position, ArrowStats.Arrow.forward, ArrowStats.AttackStats.Movement.Force);
                 else
                     path.ChangeDestination(targets[0].position, ArrowStats.AttackStats.Movement.Force);
 
+                // distance = BezireCurve.CubicDistance(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);
+
                 while (t <= 1f ) {
-                    ArrowStats.Arrow.DOMove(BezireCurve.Cubic(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t), 0f);
-                    ArrowStats.Arrow.DOLookAt(BezireCurve.Quadratic(path.Points[1], path.Points[2], path.Points[3], t), 0.2f);
+                    ArrowStats.Arrow.DOMove(BezireCurve.CubicPoint(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t), 0f);
+                    ArrowStats.Arrow.forward = BezireCurve.CubicDirection(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);
                     
                     yield return new WaitForEndOfFrame(); 
 
                     path.RecalculatePath(targets[0].position);
-
-                    t += (ArrowStats.AttackStats.Movement.Speed * Time.deltaTime) / (distance * Time.deltaTime) * Time.deltaTime;            
+     
+                    var velocity = BezireCurve.CubicVelocity(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);     
+                    var distance = BezireCurve.CubicDistance(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);
+                    t += ArrowStats.AttackStats.Movement.Speed * velocity / distance * Time.deltaTime;                
                 }
                 targets.RemoveAt(0);
             }
@@ -58,20 +59,22 @@ namespace YundosArrow.Scripts.Player.Combat.ArrowAbilities.Actions
 
             t = 0f;
             var lastPos = ArrowStats.Arrow.position;
-            distance = Vector3.Distance(lastPos, ArrowStats.StartPoint.position);
             path.ChangeDestination(ArrowStats.StartPoint.position, ArrowStats.AttackStats.Movement.Force);
+            // distance = BezireCurve.CubicDistance(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);
             while (t <= 1f ) {
-                ArrowStats.Arrow.DOMove(BezireCurve.Cubic(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t), 0f);
-                ArrowStats.Arrow.DOLookAt(BezireCurve.Quadratic(path.Points[1], path.Points[2], path.Points[3], t), 0.2f);
+                ArrowStats.Arrow.DOMove(BezireCurve.CubicPoint(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t), 0f);
+                ArrowStats.Arrow.forward = BezireCurve.CubicDirection(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);
                 
                 yield return new WaitForEndOfFrame();
                 
                 path.RecalculatePath(ArrowStats.StartPoint.position);
                 var point = ArrowStats.StartPoint.position - ArrowStats.StartPoint.forward * (ArrowStats.AttackStats.Movement.ReturnForce);
-                // point += ArrowStats.StartPoint.right * (ArrowStats.AttackStats.Movement.ReturnForce);
+                point += ArrowStats.StartPoint.right * (ArrowStats.AttackStats.Movement.ReturnForce);
                 path.MovePoint(path.Points.Length - 2, point);
 
-                t += (ArrowStats.AttackStats.Movement.Speed * Time.deltaTime) / (distance * Time.deltaTime) * Time.deltaTime;            
+                var velocity = BezireCurve.CubicVelocity(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);
+                var distance = BezireCurve.CubicDistance(path.Points[0], path.Points[1], path.Points[2], path.Points[3], t);
+                t += ArrowStats.AttackStats.Movement.Speed * velocity / distance * Time.deltaTime;            
             }
 
             ArrowStats.Arrow.parent = cacheParent;
